526c7f5810c0704d7f42a95b3a48bdd9b9b0e537
Joshua Halickman <jhalickman@gmai.com>
2015-01-28 12:46:40 -0500

    Metadata updates

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name             'cloudwatch_logs'
-maintainer       'Joshua Halickma'
+maintainer       'Joshua Halickman'
 maintainer_email 'josh@abacus.com'
 license          'All rights reserved'
-description      'Installs/Configures Amazon\'s cloudwatch_logs'
+description      'Installs and configures the AWS Cloud Watch Logs Agent.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
